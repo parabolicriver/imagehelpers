96eb1f626a59e355066c6e216636c3f745d70493
Anuj Seth <greenthunderleaf@gmail.com>
2013-02-11 18:18:20 +0530

    Fixed project names.

--- UIImage+TintExtensions.h
+++ UIImage+TintExtensions.h
@@ -1,6 +1,6 @@
 //
 //  UIImage+TintExtensions.h
-//  TabataStopwatch
+//  PARImageHelpers
 //
 //  Created by Anuj Seth on 8/12/12.
 //  Copyright (c) 2012 Parabolic River. All rights reserved.
--- UIImage+TintExtensions.m
+++ UIImage+TintExtensions.m
@@ -1,6 +1,6 @@
 //
 //  UIImage+TintExtensions.m
-//  TabataStopwatch
+//  PARImageHelpers
 //
 //  Created by Anuj Seth on 8/12/12.
 //  Copyright (c) 2012 Parabolic River. All rights reserved.
