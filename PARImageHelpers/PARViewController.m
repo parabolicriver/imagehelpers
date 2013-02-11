0839a825b4d783b20ef91517741bbf72bb2e2693
Anuj Seth <greenthunderleaf@gmail.com>
2013-02-11 19:07:55 +0530

    Added simple test cases.

--- PARImageHelpers/PARViewController.m
+++ PARImageHelpers/PARViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "PARViewController.h"
+#import "UIImageView+ImageAdditions.h"
+#import "UIImage+TintExtensions.h"
 
 @interface PARViewController ()
+
+@property (nonatomic, strong) UIImageView *normalGear;
+@property (nonatomic, strong) UIImageView *tintedGear;
 
 @end
 
@@ -18,6 +23,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // image view extensions test
+    self.normalGear = [UIImageView imageViewWithImageNamed:@"noun_project_1241"];
+    self.normalGear.frame = CGRectMake(20, 20, 100, 100);
+    [self.view addSubview:self.normalGear];
+    
+    // image tint extensions test
+    UIImage *tintedGearImage = [UIImage imageNamed:@"noun_project_1241"];
+    tintedGearImage = [tintedGearImage tintedImageUsingColor:[UIColor redColor]];
+    self.tintedGear = [[UIImageView alloc] initWithImage:tintedGearImage];
+    self.tintedGear.frame = CGRectMake(20, 140, 100, 100);
+    [self.view addSubview:self.tintedGear];
 }
 
 - (void)didReceiveMemoryWarning
