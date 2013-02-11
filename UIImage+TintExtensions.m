85c9e0f1f2127617b390805cb194fcb612a1e3d9
Anuj Seth <greenthunderleaf@gmail.com>
2013-02-11 18:19:53 +0530

    Cleaned up code formatting a bit.

--- UIImage+TintExtensions.m
+++ UIImage+TintExtensions.m
@@ -12,12 +12,15 @@
 
 - (UIImage*)tintedImageUsingColor:(UIColor*)tintColor {
     UIGraphicsBeginImageContext(self.size);
+    
     CGRect drawRect = CGRectMake(0, 0, self.size.width, self.size.height);
     [self drawInRect:drawRect];
     [tintColor set];
     UIRectFillUsingBlendMode(drawRect, kCGBlendModeSourceAtop);
     UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
+    
     UIGraphicsEndImageContext();
+    
     return tintedImage;
 }
 
