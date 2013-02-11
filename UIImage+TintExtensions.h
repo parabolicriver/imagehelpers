dc7fadb3690ad78c04435c8c3abbecaa3fe7c84d
Anuj Seth <greenthunderleaf@gmail.com>
2013-02-11 18:17:41 +0530

    Added UIImage tint helpers from tabata stopwatch.

---
+++ UIImage+TintExtensions.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+TintExtensions.h
+//  TabataStopwatch
+//
+//  Created by Anuj Seth on 8/12/12.
+//  Copyright (c) 2012 Parabolic River. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (TintExtensions)
+
+- (UIImage*)tintedImageUsingColor:(UIColor*)tintColor;
+
+@end
---
+++ UIImage+TintExtensions.m
@@ -0,0 +1,24 @@
+//
+//  UIImage+TintExtensions.m
+//  TabataStopwatch
+//
+//  Created by Anuj Seth on 8/12/12.
+//  Copyright (c) 2012 Parabolic River. All rights reserved.
+//
+
+#import "UIImage+TintExtensions.h"
+
+@implementation UIImage (TintExtensions)
+
+- (UIImage*)tintedImageUsingColor:(UIColor*)tintColor {
+    UIGraphicsBeginImageContext(self.size);
+    CGRect drawRect = CGRectMake(0, 0, self.size.width, self.size.height);
+    [self drawInRect:drawRect];
+    [tintColor set];
+    UIRectFillUsingBlendMode(drawRect, kCGBlendModeSourceAtop);
+    UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return tintedImage;
+}
+
+@end
